23d566a74b82e7620d3179966f9d7b10a0eb2fe4
Faizan Khan <faizankhan062002@gmail.com>
2026-01-31 18:02:22 +0530

    Remove the `name` field from the model schema.

--- src/db/models.ts
+++ src/db/models.ts
@@ -8,7 +8,6 @@ const mcpServerSchema = new mongoose.Schema({
     args: { type: [String], required: false }, // For Stdio
     env: { type: Map, of: String, required: false }, // For Stdio
     type: { type: String, required: true, enum: ["sse", "http", "stdio"] },
-    name: { type: String, required: false },
     logo: { type: String, required: false },
     isActive: { type: Boolean, default: true },
     addedAt: { type: Date, default: Date.now }
